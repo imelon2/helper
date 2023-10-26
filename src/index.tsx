d243876194fb692dc0ec7d439e2caffdf2de58cd
imelon2 <imelon279@naver.com>
2023-10-26 09:49:23 +0900

    basename

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter basename='https://imelon2.github.io/helper'>
+  <BrowserRouter basename='/helper/'>
     <App />
   </BrowserRouter>
 );
